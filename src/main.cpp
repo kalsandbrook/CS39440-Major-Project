1dacf35b31531c09c8ca2d86811ad55b53c85941
Kal Sandbrook <kas143@aber.ac.uk>
2024-02-19 12:50:32 +0000

    Add breeze theme and check if WIN32

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <QApplication>
-#include <QPushButton>
+#include <QFile>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+#ifdef WIN32
+    Q_INIT_RESOURCE(breeze);
+
+    QFile file(":/dark/stylesheet.qss");
+    file.open(QFile::ReadOnly | QFile::Text);
+    QTextStream stream(&file);
+    app.setStyleSheet(stream.readAll());
+#endif
 
     MainWindow window;
 
