e504f5801c91c8d8d1e990fb14bc2ea4d940bd22
Kal Sandbrook <kas143@aber.ac.uk>
2024-03-09 03:53:36 +0000

    Rename AddGameDialog to GameEditDialog

---
+++ src/gameeditdialog.cpp
@@ -0,0 +1,136 @@
+//
+// Created by Kal on 24/02/2024.
+//
+
+#include <QGridLayout>
+#include <QMessageBox>
+#include <QStringList>
+
+#include "gameeditdialog.h"
+#include "data/game.h"
+#include "data/gamelibrary.h"
+
+GameEditDialog::GameEditDialog(QWidget* parent)
+        : QDialog(parent)
+        , editingGame(false)
+{
+    nameLabel = new QLabel(tr("Name:"));
+    nameLineEdit = new QLineEdit(this);
+
+    descLabel = new QLabel(tr("Description:"));
+    descTextEdit = new QTextEdit(this);
+
+    genreLabel = new QLabel(tr("Genres:"));
+    genreList = new QListWidget(this);
+
+    populateGenreList(genreList);
+
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &GameEditDialog::verify);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &GameEditDialog::reject);
+
+    auto* mainLayout = new QGridLayout;
+    mainLayout->addWidget(nameLabel, 0, 0);
+    mainLayout->addWidget(nameLineEdit, 0, 1);
+    mainLayout->addWidget(descLabel, 1, 0);
+    mainLayout->addWidget(descTextEdit, 1, 1);
+    mainLayout->addWidget(genreLabel, 2, 0);
+    mainLayout->addWidget(genreList, 2, 1, 3, 2);
+    mainLayout->addWidget(buttonBox, 5, 1, 1, 2);
+
+    resize(400, 300);
+
+    setLayout(mainLayout);
+}
+
+void GameEditDialog::populateGenreList(QListWidget* genreList)
+{
+    QStringList genres {};
+    genres << tr("Action") << tr("Adventure") << tr("Arcade") << tr("Board")
+           << tr("Card") << tr("Casino") << tr("Educational") << tr("Family")
+           << tr("Fighting") << tr("Music") << tr("Platform") << tr("Puzzle")
+           << tr("Racing") << tr("Role-Playing") << tr("Shooter")
+           << tr("Simulation") << tr("Sports") << tr("Strategy") << tr("Trivia")
+           << tr("Word");
+    genreList->addItems(genres);
+}
+
+void GameEditDialog::verify()
+{
+    if (!nameLineEdit->text().isEmpty() && !descTextEdit->toPlainText().isEmpty()) {
+        if (!genreList->selectedItems().empty()) {
+            accept();
+        }
+    } else {
+        QMessageBox::information(this, tr("Error"),
+                                 tr("Please fill in all fields."));
+    }
+}
+
+void GameEditDialog::accept()
+{
+    GameLibrary& gameLibrary = GameLibrary::instance();
+
+    if(editingGame){
+        editedGame.setName(name());
+        editedGame.setDesc(desc());
+        editedGame.setGenres(genre());
+        gameLibrary.updateGame(editedGame);
+    } else {
+        Game newGame(0, name(), desc(), genre());
+        gameLibrary.addGame(newGame);
+    }
+    QDialog::accept();
+}
+
+QString GameEditDialog::name() const { return nameLineEdit->text(); }
+
+QString GameEditDialog::desc() const { return descTextEdit->toPlainText(); }
+
+QStringList GameEditDialog::genre() const
+{
+    QStringList genres;
+    QList<QListWidgetItem*> items = genreList->selectedItems();
+    for (QListWidgetItem* item : items) {
+        genres << item->text();
+    }
+    return genres;
+}
+
+int GameEditDialog::exec()
+{
+    editingGame = false;
+    setWindowTitle(tr("Add New Game"));
+
+    nameLineEdit->clear();
+    descTextEdit->clear();
+    genreList->clearSelection();
+
+    return QDialog::exec();
+}
+
+void GameEditDialog::setGameToEdit(const Game &game){
+    editedGame = game;
+    nameLineEdit->setText(game.name());
+    descTextEdit->setText(game.desc());
+
+    QStringList genres = game.genres();
+    for (int i = 0; i < genreList->count();++i) {
+        QListWidgetItem *item = genreList->item(i);
+        item->setSelected(genres.contains(item->text()));
+    }
+}
+
+
+int GameEditDialog::exec(int gameId) {
+    editingGame = true;
+
+    setWindowTitle(tr("Editing Game"));
+
+    auto game = GameLibrary::instance().getGameById(gameId);
+
+    setGameToEdit(game);
+
+    return QDialog::exec();
+}
