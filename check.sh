84224f8822e9fe5f2514bc1bd3e8e57e32b217ed
Kal Sandbrook <kas143@aber.ac.uk>
2024-04-22 20:47:19 +0100

    shellcheck for check.sh

--- check.sh
+++ check.sh
@@ -2,5 +2,5 @@
 
 # Run clang-format on all .h and .cpp files.
 
-find ./src/ -iname *.h -o -iname *.cpp | xargs clang-format -i
+find ./src/ -iname "*.h" -print0 -o -iname "*.cpp"  | xargs -0 clang-format -i
 
